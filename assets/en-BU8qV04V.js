3e967eba5ebf0957e59cedfa1be747a25d15109a
Madamsetty Suman Kumar <madamsettykumar@gmail.com>
2025-06-19 17:26:14 +0300

    Updates

---
+++ assets/en-BU8qV04V.js
@@ -0,0 +1,17 @@
+const t="HOW MUCH COULD YOU HELP?",e="Touch screen to play",n="Select Language",o="Information",i="Settings",s="Information",r="This is an interactive landing page with a 3D sphere. Move your mouse or touch the screen to interact with the interface. After 5 seconds of inactivity, the menu will expand.",a="Settings",c="Configure your preferences here. You can adjust various options to customize your experience.",h="Sphere Information",p="What is this sphere?",T="This is an interactive 3D sphere rendered using Three.js. It rotates continuously and responds to user interactions.",u="Click on the sphere to learn more about its properties and behavior.",I="Quiz question image",O="Option A",l="Option B",E="Option C",A="Option D",R="This is the about section. It explains what this application does and who made it.",g="The MIT License",S="Copyright © 2010-2024 three.js authors",f=`Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.`,N={title:t,touchSwipeHint:e,selectLanguage:n,info:o,settings:i,infoTitle:s,infoContent:r,settingsTitle:a,settingsContent:c,sphereTitle:h,sphereQuestion:p,sphereDescription:T,sphereDetails:u,sphereImageAlt:I,optionA:O,optionB:l,optionC:E,optionD:A,aboutContent:R,mitLicense:g,mitCopyright:S,mitLicenseText:f};export{R as aboutContent,N as default,o as info,r as infoContent,s as infoTitle,S as mitCopyright,g as mitLicense,f as mitLicenseText,O as optionA,l as optionB,E as optionC,A as optionD,n as selectLanguage,i as settings,c as settingsContent,a as settingsTitle,T as sphereDescription,u as sphereDetails,I as sphereImageAlt,p as sphereQuestion,h as sphereTitle,t as title,e as touchSwipeHint};
